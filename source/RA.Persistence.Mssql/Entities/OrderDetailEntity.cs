31981cffe1f7f88de839b6b08e427db7e1c384d0
LOGOMERKEZ\Huseyin.Yilmaz <huseyin.yilmaz@logo.com.tr>
2019-06-08 20:41:14 +0300

    Table properties.

--- source/RA.Persistence.Mssql/Entities/OrderDetailEntity.cs
+++ source/RA.Persistence.Mssql/Entities/OrderDetailEntity.cs
@@ -1,8 +1,10 @@
 ﻿using RA.Persistence.Interfaces;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RA.Persistence.Mssql.Entities
 {
+    [Table("OrderDetail")]
     public class OrderDetailEntity : BaseEntity
     {
         public int Id { get; set; }
--- source/RA.Persistence.Mssql/Entities/OrderEntity.cs
+++ source/RA.Persistence.Mssql/Entities/OrderEntity.cs
@@ -1,8 +1,10 @@
 ﻿using RA.Persistence.Interfaces;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RA.Persistence.Mssql.Entities
 {
+    [Table("Order")]
     public class OrderEntity : BaseEntity
     {
         public int Id { get; set; }
