83607f45a3786b6e1885ab86c4785efeb31f3330
huseyin.yilmaz <Huseyin.Yilmaz@logo.com.tr>
2019-06-21 23:11:30 +0300

    Login window arrangements.

--- source/RA.WindowsClient/MainWindow.xaml.cs
+++ source/RA.WindowsClient/MainWindow.xaml.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RA.WindowsClient
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void KeyPad_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Password.Length < txtPassword.MaxLength)
+            {
+                Button button = sender as Button;
+                txtPassword.Password = string.Concat(txtPassword.Password, button.ToolTip);
+            }
+        }
+
+        private void DeleteKey_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Password.Length != 0)
+                txtPassword.Password = txtPassword.Password.Substring(0, txtPassword.Password.Length - 1);
         }
     }
 }
