1afbc678adaccd730a3b0ea0200c1d3f09738eb2
huseyin.yilmaz <Huseyin.Yilmaz@logo.com.tr>
2019-06-23 12:11:53 +0300

    Global exception handling.

--- source/RA.WindowsClient/App.xaml.cs
+++ source/RA.WindowsClient/App.xaml.cs
@@ -14,5 +14,11 @@ namespace RA.WindowsClient
         {
             Resource.Culture = Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
         }
+
+        private void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+            e.Handled = true;
+        }
     }
 }
