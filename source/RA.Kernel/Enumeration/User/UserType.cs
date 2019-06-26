455bc2ad9785d82177c9a1f03821e551cfb5ab68
huseyin.yilmaz <Huseyin.Yilmaz@logo.com.tr>
2019-06-26 20:39:11 +0300

    View model arrangements.

--- source/RA.Kernel/Enumeration/User/UserType.cs
+++ source/RA.Kernel/Enumeration/User/UserType.cs
@@ -1,8 +1,12 @@
-﻿namespace RA.Kernel.Enumeration.User
+﻿using RA.Kernel.Attributes;
+
+namespace RA.Kernel.Enumeration.User
 {
     public enum UserType
     {
+        [Resource("Admin")]
         Admin = 0,
+        [Resource("Android")]
         Anroid = 1,
     }
 }
---
+++ source/RA.WindowsClient/Common/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using RA.WindowsClient.Attributes;
+using System;
+using System.Windows;
+
+namespace RA.WindowsClient.Common
+{
+    public class BaseViewModel
+    {
+        [ListView("", Visibility.Hidden)]
+        public int Id { get; set; }
+
+        [ListView("", Visibility.Hidden)]
+        public DateTime CreatedDate { get; set; }
+
+        [ListView("", Visibility.Hidden)]
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
